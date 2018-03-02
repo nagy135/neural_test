9d41c214ac127ad36994fbc246bc31d213365e9a
nagy135 <viktor.nagy1995@gmail.com>
2018-03-02 10:58:46 +0100

    first working neural net

--- data_creator.py
+++ data_creator.py
@@ -3,7 +3,8 @@ import numpy as np
 def create_data( num_records, num_bits ):
 	data = dict()
 	data['X'] = np.random.randint(2, size=(num_records, num_bits))
-	data['y'] = np.array([list("{0:b}".format(int(''.join([str(x) for x in row]), 2) + 1).zfill(num_bits))[-num_bits:] for row in data['X']], dtype='int64')
+	data['y'] = np.array([list("{0:b}".format(int(''.join([str(x) for x in row]), 2) + 1).zfill(num_bits))[-num_bits:] for row in data['X']], dtype='float64')
+	data['X'] = data['X'].astype('float64')
 	return data
 if __name__ == '__main__':
     print(create_data(15,5))
--- neural.py
+++ neural.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from data_creator import create_data
 
 n_nodes_hl1 = 500
 n_nodes_hl2 = 500
@@ -9,23 +10,21 @@ n_bits = 10
 n_classes = 10
 batch_size = 100
 
-x = tf.placeholder('float', [None, n_bits])
-y = tf.placeholder('float')
+place_x = tf.placeholder('float64', [None, n_bits])
+place_y = tf.placeholder('float64', [None, n_bits])
 
 def neural_network_model(data):
-    print(data.shape)
-    hidden_1_layer = {'weights':tf.Variable(tf.random_normal([n_bits, n_nodes_hl1])),
-                      'biases':tf.Variable(tf.random_normal([n_nodes_hl1]))}
-    print(hidden_1_layer['weights'])
+    hidden_1_layer = {'weights':tf.Variable(tf.random_normal([n_bits, n_nodes_hl1], dtype='float64')),
+                      'biases':tf.Variable(tf.random_normal([n_nodes_hl1], dtype='float64'))}
 
-    hidden_2_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])),
-                      'biases':tf.Variable(tf.random_normal([n_nodes_hl2]))}
+    hidden_2_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2], dtype='float64')),
+                      'biases':tf.Variable(tf.random_normal([n_nodes_hl2], dtype='float64'))}
 
-    hidden_3_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl2, n_nodes_hl3])),
-                      'biases':tf.Variable(tf.random_normal([n_nodes_hl3]))}
+    hidden_3_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl2, n_nodes_hl3], dtype='float64')),
+                      'biases':tf.Variable(tf.random_normal([n_nodes_hl3], dtype='float64'))}
 
-    output_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl3, n_classes])),
-                    'biases':tf.Variable(tf.random_normal([n_classes])),}
+    output_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl3, n_bits], dtype='float64')),
+                    'biases':tf.Variable(tf.random_normal([n_bits], dtype='float64')),}
 
 
     l1 = tf.add(tf.matmul(data,hidden_1_layer['weights']), hidden_1_layer['biases'])
@@ -41,13 +40,13 @@ def neural_network_model(data):
 
     return output
 
-def train_neural_network(x):
-    prediction = neural_network_model(x)
+def train_neural_network(place_x):
+    prediction = neural_network_model(place_x)
     print('neural network created')
     # OLD VERSION:
     #cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(prediction,y) )
     # NEW:
-    cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y) )
+    cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=place_y) )
     optimizer = tf.train.AdamOptimizer().minimize(cost)
     
     hm_epochs = 10
@@ -59,18 +58,21 @@ def train_neural_network(x):
 
         for epoch in range(hm_epochs):
             epoch_loss = 0
-            for _ in range(int(mnist.train.num_examples/batch_size)):
-                epoch_x, epoch_y = mnist.train.next_batch(batch_size)
-                _, c = sess.run([optimizer, cost], feed_dict={x: epoch_x, y: epoch_y})
-                epoch_loss += c
+            feed_dict_temp = dict()
+            generated_data = create_data(70000, n_bits)
+            for i, item in enumerate(generated_data['X']):
+                print(item)
+                print(generated_data['y'][i])
+                print()
+            _, c = sess.run([optimizer, cost], feed_dict={place_x: generated_data['X'], place_y: generated_data['y']})
+            epoch_loss += c
 
             print('Epoch', epoch, 'completed out of',hm_epochs,'loss:',epoch_loss)
 
-        correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
+        correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(place_y, 1))
 
         accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
-        print('Accuracy:',accuracy.eval({x:mnist.test.images, y:mnist.test.labels}))
+        generated_data_test = create_data(3000, n_bits)
+        print('Accuracy:',accuracy.eval({place_x:generated_data_test['X'], place_y:generated_data_test['y']}))
 
-from data_creator import create_data
-x = create_data( 5000, n_bits )
-train_neural_network(x)
+train_neural_network(place_x)
