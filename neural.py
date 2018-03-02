c089f5e1a0798398c2af4e2266a1caf8aeee47c5
nagy135 <viktor.nagy1995@gmail.com>
2018-03-02 11:25:22 +0100

    casual edit

--- neural.py
+++ neural.py
@@ -1,13 +1,13 @@
 import tensorflow as tf
 from data_creator import create_data
 
-n_nodes_hl1 = 500
-n_nodes_hl2 = 500
-n_nodes_hl3 = 500
+n_nodes_hl1 = 1000
+n_nodes_hl2 = 1000
+n_nodes_hl3 = 1000
+n_nodes_hl4 = 1000
 
-n_bits = 10
+n_bits = 20
 
-n_classes = 10
 batch_size = 100
 
 place_x = tf.placeholder('float64', [None, n_bits])
@@ -23,7 +23,10 @@ def neural_network_model(data):
     hidden_3_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl2, n_nodes_hl3], dtype='float64')),
                       'biases':tf.Variable(tf.random_normal([n_nodes_hl3], dtype='float64'))}
 
-    output_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl3, n_bits], dtype='float64')),
+    hidden_4_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl3, n_nodes_hl4], dtype='float64')),
+                      'biases':tf.Variable(tf.random_normal([n_nodes_hl4], dtype='float64'))}
+                      
+    output_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl4, n_bits], dtype='float64')),
                     'biases':tf.Variable(tf.random_normal([n_bits], dtype='float64')),}
 
 
@@ -36,7 +39,10 @@ def neural_network_model(data):
     l3 = tf.add(tf.matmul(l2,hidden_3_layer['weights']), hidden_3_layer['biases'])
     l3 = tf.nn.relu(l3)
 
-    output = tf.matmul(l3,output_layer['weights']) + output_layer['biases']
+    l4 = tf.add(tf.matmul(l3,hidden_4_layer['weights']), hidden_4_layer['biases'])
+    l4 = tf.nn.relu(l4)
+
+    output = tf.matmul(l4,output_layer['weights']) + output_layer['biases']
 
     return output
 
@@ -47,9 +53,9 @@ def train_neural_network(place_x):
     #cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(prediction,y) )
     # NEW:
     cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=place_y) )
-    optimizer = tf.train.AdamOptimizer().minimize(cost)
+    optimizer = tf.train.AdamOptimizer(0.001).minimize(cost)
     
-    hm_epochs = 10
+    hm_epochs = 30
     with tf.Session() as sess:
         # OLD:
         #sess.run(tf.initialize_all_variables())
@@ -59,11 +65,7 @@ def train_neural_network(place_x):
         for epoch in range(hm_epochs):
             epoch_loss = 0
             feed_dict_temp = dict()
-            generated_data = create_data(70000, n_bits)
-            for i, item in enumerate(generated_data['X']):
-                print(item)
-                print(generated_data['y'][i])
-                print()
+            generated_data = create_data(5000, n_bits)
             _, c = sess.run([optimizer, cost], feed_dict={place_x: generated_data['X'], place_y: generated_data['y']})
             epoch_loss += c
 
